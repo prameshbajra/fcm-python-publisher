b3924bdb1686813134ab3291574d856981222be9
prameshbajra <pe.messh@gmail.com>
2023-01-09 11:00:30 +0545

    Multiple calls at once to app by multithreading.

--- messaging.py
+++ messaging.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 from firebase_admin import credentials, initialize_app, messaging
 
 cred = credentials.Certificate(
@@ -7,21 +8,37 @@ app = initialize_app(cred)
 
 print('PROJECT ID: ', app.project_id)
 
-message = messaging.MulticastMessage(
-    data={
-        'testId': '995971',
-        'destination': '977981345782',
-        'text': 'TP-5971',
-        'simSlot': '0',
-        'simId': 'SimOne',
-        'type': 'sentSms'
-    },
-    tokens=[
-        'cGpJ-f5AR5ez8t7nP87gVr:APA91bGjyom_rbN_0JBTMZDL5etebyKS0p5k43pm8FukUL1IIqokUaQ9AmHuiF4nu39ZOo60iHdJ3hPIcPO0nC9nhv-hzMZTddohcqiWdRBY3I2LBEFxxNxHgz4mW8tcTWnm3EXN89nQ',
-    ])
+tasks = []
 
-message_response = messaging.send_multicast(multicast_message=message)
-print(message_response)
+for i in range(100, 103):
+    message = messaging.MulticastMessage(
+        data={
+            'testId': f'{i}335971',
+            'destination': '9779813457822',
+            'text': f'{i}-TP-5971',
+            'simSlot': '0',
+            'simId': 'SimOne',
+            'type': 'sentSms'
+        },
+        tokens=[
+            'cGpJ-f5AR5ez8t7nP87gVr:APA91bGjyom_rbN_0JBTMZDL5etebyKS0p5k43pm8FukUL1IIqokUaQ9AmHuiF4nu39ZOo60iHdJ3hPIcPO0nC9nhv-hzMZTddohcqiWdRBY3I2LBEFxxNxHgz4mW8tcTWnm3EXN89nQ',
+        ])
 
-print('Failure Count : ', message_response.failure_count)
-print('Responses (Probably not of any use): ', message_response.responses)
+    tasks.append(message)
+
+
+def send(message):
+    print("SEND : ", message)
+    message_response = messaging.send_multicast(multicast_message=message)
+    print(message_response)
+    print('Failure Count : ', message_response.failure_count)
+    print('Responses (Probably not of any use): ', message_response.responses)
+    print('----------')
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    # Start the tasks
+    results = [executor.submit(send, task) for task in tasks]
+
+    # Wait for the tasks to complete
+    concurrent.futures.wait(results)
